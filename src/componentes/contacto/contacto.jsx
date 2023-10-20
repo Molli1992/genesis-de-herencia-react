28925b3e903ac297f9ae0258e113f01239df88d7
molli1992 <fblaksley@rukasvancor.com>
2023-10-20 14:22:52 -0300

    solo falta calidad de imagenes

--- src/componentes/contacto/contacto.jsx
+++ src/componentes/contacto/contacto.jsx
@@ -96,7 +96,13 @@ function Contacto() {
         <div className="container-left-contacto">
           <h1>Canales de Contacto</h1>
 
-          <div style={{ marginTop: "50px" }}>
+          <div
+            style={{
+              marginTop: "50px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -110,21 +116,8 @@ function Contacto() {
                 d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
               />
             </svg>
-            <h3>+1 000-00-00</h3>
-          </div>
 
-          <div style={{ marginTop: "10px" }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="#858585"
-              class="bi bi-envelope-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-            </svg>
-            <h3>contacto@marceloruiz.com</h3>
+            <h3 style={{ height: "10px" }}>+54 9 11 6783-9590</h3>
           </div>
 
           <h1 style={{ marginTop: "100px" }}>Siguenos</h1>
